3c3dc252447f08b20f4f0adbb396e0f55f5eafe8
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-03-21 14:00:56 +0100

    start

---
+++ etcd.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "etcdv3" {
+    endpoints = ["localhost:2379"]
+    lock      = true
+    prefix    = "covid-videoplattform/terraform-state/"
+  }
+}
---
+++ testvm.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
