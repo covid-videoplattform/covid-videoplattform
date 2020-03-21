74e9c314bb4d374d4255916fc832803e6e03aadc
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-03-21 14:36:05 +0100

    network subnet

--- network.tf
+++ network.tf
@@ -3,3 +3,7 @@ resource "google_compute_network" "videoplattform_network" {
   name                    = "videoplattform-network"
   auto_create_subnetworks = "true"
 }
+
+data "google_compute_subnetwork" "videoplattform_subnetwork" {
+  name   = "videoplattform-network"
+}
