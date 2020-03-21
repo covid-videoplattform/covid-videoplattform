301e6032d39ec44714683cb168fe51dc9f296d40
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-03-21 16:22:28 +0100

    more infrastructure

---
+++ infrastructure/prosody.tf
@@ -0,0 +1,26 @@
+resource "google_compute_address" "prosody_ip_address" {
+  name = "prosody-ip-address"
+}
+
+
+resource "google_compute_instance" "prosody" {
+  name         = "prosody"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.debian_image.self_link
+    }
+  }
+
+  metadata = {
+    ssh-keys = "root:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwdEkFBdQfY5YB6LR1l+copG7rZXlGLQyWWwhZdNkpW"
+  }
+
+  network_interface {
+    network = google_compute_network.videoplattform_network.self_link
+    access_config {
+      nat_ip = google_compute_address.prosody_ip_address.address
+    }
+  }
+}
