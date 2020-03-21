cbe8f81267a73267539b00c3d2ca3a7054699bf8
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-03-21 17:24:01 +0100

    infrastructure connection available

--- infrastructure/bastion.tf
+++ infrastructure/bastion.tf
@@ -13,6 +13,10 @@ resource "google_compute_instance" "bastion" {
     }
   }
 
+  metadata = {
+    ssh-keys = "root:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwdEkFBdQfY5YB6LR1l+copG7rZXlGLQyWWwhZdNkpW"
+  }
+
   network_interface {
     network = google_compute_network.videoplattform_network.self_link
     access_config {
