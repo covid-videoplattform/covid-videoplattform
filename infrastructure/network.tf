5112bd49f18e909552fe457a992fcd67b895261c
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-03-22 01:39:51 +0100

    disable gce firewall

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -13,11 +13,6 @@ resource "google_compute_firewall" "allow" {
   network = google_compute_network.videoplattform_network.name
 
   allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443", "8080", "1000-2000","22"]
+    protocol = "all"
   }
 }
